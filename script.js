66f5ae66c3c9650ee8e03bb72a06e54dd93df59a
twills <treav.wills@gmail.com>
2022-06-29 01:16:58 -0600

    redirect the user if they don't select any characters to use

--- script.js
+++ script.js
@@ -13,17 +13,42 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
+
+  // define the character strings we can use
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var specials = "~`!@#$%^&*()-_+=}]{[:;<,>.?/";
+
+  // set length equal to the length determined by passwordLength()
   var length = passwordLength();
+
+  // print the length to the console for debugging/development
   console.log("Password length: " + length);
 
+  // create array to hold the different types of characters desired in a password
   var characterTypes = ["lowercase", "uppercase", "numeric", "special"]
 
+  // create array equal in length to characterTypes[]
   var includeArray = new Array(characterTypes.length);
 
+  // fill the includeArray with booleans corresponding to the characterTypes
+  
   for (i=0; i<characterTypes.length; i++) {
     includeArray[i] = passwordInclude(characterTypes[i]);
     console.log("Include " + characterTypes[i] + " : " + includeArray[i]);  
   }
+
+  var numberOfCharacterTypes = 0;
+  for (i = 0; i < includeArray.length; i++) {
+    numberOfCharacterTypes += includeArray[i];
+  }
+
+  if (numberOfCharacterTypes === 0){
+    alert("You declined all characters, please begin again.");
+    return "Try Again!";
+  }
+  
+  //create a string the length of the password required for each character type desired
+
 
   return "poop";
 }
